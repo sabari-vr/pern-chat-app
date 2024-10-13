772aa5993db07faa667809039a0396a52be184f9
Sabari VR <sabari.r@nergyindia.com>
2024-10-13 11:23:30 +0530

    added dir

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "development") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
