209fd9bd87371f0d19a2ab9ee3bf418887d3e01c
Sabari VR <sabari.r@nergyindia.com>
2024-11-01 12:56:48 +0530

    new cron job url

--- backend/src/cron.js
+++ backend/src/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://pern-chat-app-92f2.onrender.com";
+const URL = "https://pern-chat-app-92f2.onrender.com/login";
 
 const job = new cron.CronJob("*/10 * * * *", function () {
   https
